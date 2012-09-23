6eed6f8a45de1389992936caa3c626dcd2d54e64
Jeremy Walker <jeremy@meducation.net>
2012-09-23 18:54:15 +0100

    Select the correct menu item

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  def menu_item_class(item)
+    active = case item
+      when :speakers 
+        controller_name == "speakers"
+      else
+        controller_name == "pages" && action_name == "index"
+      end
+    active ? "active" : nil
+  end
+  
 end
