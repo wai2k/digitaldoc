536d007e1da9da87ffecc1c4897c7380ad06df98
Stevan C Wing <stevancw@me.com>
2013-08-16 17:46:47 +0100

    Sort speakers by id rather than updated_at

--- app/controllers/speakers_controller.rb
+++ app/controllers/speakers_controller.rb
@@ -5,7 +5,8 @@ class SpeakersController < ApplicationController
   # GET /speakers
   # GET /speakers.json
   def index
-    @speakers = Speaker.all
+    @speakers = Speaker.find(:all, :order => 'id')
+    
 
     respond_to do |format|
       format.html # index.html.erb
